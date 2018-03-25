6f77cb12d77baa09b5edfa31d0e84c393d44e20a
Ganesha Danu <ganeshadanu1@gmail.com>
2018-03-25 14:14:27 +0700

    defining operations

--- header.h
+++ header.h
@@ -3,8 +3,14 @@
 
 // Celebrity Data Structure
 
+#define first(L) (L).first
+#define last(L)  (L).last
+#define next(P)  (P)->next
+#define prev(P)  (P)->prev
+
 struct infotypeCeleb {
     std::string username;
+    listFollowers Follower;
 };
 
 typedef struct Celeb *addressCeleb;
@@ -19,10 +25,18 @@ typedef struct listCelebs {
 };
 
 
+addressCeleb GetCeleb (listCelebs Celebs, addressCeleb);
+addressCeleb DeleteCeleb (listCelebs Celebs, addressCeleb);
+
+void InsertCeleb (listCelebs Celebs, addressCeleb);
+void CreateListCeleb (listCelebs);
+
+
 // Followers Data Structure
 
 struct infotypeFollower {
     std::string username;
+    listCelebs Following;
 };
 
 typedef struct Follower *addressFollower;
