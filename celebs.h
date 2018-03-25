caa46714328db9589539fb545142749f9406e391
Ganesha Danu <ganeshadanu1@gmail.com>
2018-03-25 14:23:33 +0700

    separating files

---
+++ celebs.h
@@ -0,0 +1,35 @@
+#include <string>
+
+
+// Celebrity Data Structure
+
+#define first(L) (L).first
+#define last(L)  (L).last
+#define next(P)  (P)->next
+#define prev(P)  (P)->prev
+#define info(P)  (P)->info
+
+struct infotypeCeleb {
+    std::string username;
+    listFollowers Follower;
+};
+
+typedef struct Celeb *addressCeleb;
+struct Celeb {
+    infotypeCeleb info;
+    addressCeleb next; 
+};
+
+typedef struct listCelebs {
+    addressCeleb first;
+    addressCeleb last;
+};
+
+addressCeleb AllocateCeleb (std::string username);
+addressCeleb GetCeleb (listCelebs Celebs, addressCeleb);
+addressCeleb DeleteCeleb (listCelebs Celebs, addressCeleb);
+
+void DeallocateCeleb (std::string username);
+void InsertCeleb (listCelebs Celebs, addressCeleb);
+void CreateListCeleb (listCelebs);
+
---
+++ followers.h
@@ -0,0 +1,20 @@
+#include <string>
+
+// Followers Data Structure
+
+struct infotypeFollower {
+    std::string username;
+    listCelebs Following;
+};
+
+typedef struct Follower *addressFollower;
+struct Follower {
+    infotypeFollower info;
+    addressFollower next; 
+};
+
+typedef struct listFollowers {
+    addressFollower first;
+    addressFollower last;
+};
+
