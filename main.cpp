3379732a51199eba1cdc3262da761b28a24e2df2
Ganesha Danu <ganeshadanu1@gmail.com>
2018-04-05 20:40:16 +0700

    creating the main loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,55 @@
 #include "header.h"
+#include "userInterface.h"
+#include "clrscr.h"
 
 using namespace std;
 
+int state = 0;
+/*
+    -1 : exit
+     0  : select mode
+     1  : celeb mode [signup]
+     2  : follower mode [signup]
+*/
+
+void showMenu () {
+    cout << "==============Welcome==============\n\n";
+    string choice[5] = {
+        "Signup as Celeb", 
+        "Signup as Follower",
+        "Login as Celeb",
+        "Login as Follower",
+        "Exit"
+        };
+
+    state = multiChoice(choice, 5);
+    if (state == 5) state = -1;
+    clrscr();
+}
+
+void CelebSignUp () {
+    cout << "===================================\n\n";
+    
+}
+
 int main () {
 
+
+    while(state != -1) {
+        cout << '\n';
+        switch (state) {
+            case 0:
+                showMenu();
+                
+                break;
+            case 1:
+                cout << "this is celeb signup";
+                break;
+            default:
+                state = -1;
+                break;
+        }
+    }
+
+    return 0;
 }
--- userInterface.cpp
+++ userInterface.cpp
@@ -1,4 +1,4 @@
-#include "userinterface.h"
+#include "userInterface.h"
 
 
 std::string getString () {
@@ -7,3 +7,16 @@ std::string getString () {
     return res;
 }
 
+int multiChoice (std::string choice[], int Nchoice) {
+    
+    int res;
+    
+    for( int i = 0; i < Nchoice; i++) {
+        std::cout << i + 1 << ". " << choice[i] << '\n';
+    }
+
+    std::cout << "\n[input]> ";
+    std::cin >> res;
+
+    return res;
+}
