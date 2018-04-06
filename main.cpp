146ca36d2c5a2daee5ffebbca8a4973c00ff19ef
Ganesha Danu <ganeshadanu1@gmail.com>
2018-04-07 00:37:09 +0700

    add admin menu

--- main.cpp
+++ main.cpp
@@ -52,46 +52,79 @@ void Continue() {
 int main () {
     clrscr();
     init();
-    string menu[9] = {
-        "Add new Celeb",
-        "Delete a Celeb",
+    string menu[7] = {
+        "Admin Menu",
         "Show all Celeb",
         "Search A Follower",
         "Top Celeb",
-        "Add new Follower",
         "Select Follower to follow Celeb",
         "Show Follower with more than 3 celebs",
         "Exit"
     };
     cout << "Choose the mode\n";
-    state = multiChoice(menu, 9);
+    state = multiChoice(menu, 7);
 
     while (state != -1) {
         
         if (state == 0){
             cout << "Choose the mode\n";
-            state = multiChoice(menu, 9);
+            state = multiChoice(menu, 7);
         }else
         if (state == 1) {
-            InsertLastCeleb (L_C, AllocateCeleb(getString("Masukan username celeb baru : ")));        
+            clrscr();
+            string submenu[5] = {
+                "Add Celeb",
+                "Delete Celeb",
+                "Add Follower",
+                "Delete Follower",
+                "Exit"
+            };
+            int substate = multiChoice(submenu, 5);
+            clrscr();
+            while(substate != -1) {
+                if (substate == 0) {
+                    substate = multiChoice(submenu, 5);
+                }
+                if (substate == 1) {
+                    InsertLastCeleb (L_C, AllocateCeleb(getString("Masukan username celeb baru : ")));
+                    substate = 0;
+                } else
+                if (substate == 2) {
+                    string username = getString("masukan username celeb yang akan dihapus : ");
+                    DeleteCeleb(L_C, GetCeleb(L_C, username));
+                    addressFollower P = first (L_F);
+                    while (P != nullptr) {
+                        DeleteCeleb(info(P).Following, GetCeleb(info(P).Following, username));    
+                        P = next(P);            
+                    }
+                    substate = 0;
+                } else
+                if (substate == 3) {
+                    InsertLastFollower (L_F, AllocateFollower(getString("Masukan username follower baru : ")));
+                    substate = 0;
+                } else
+                if (substate == 4) {
+                    string username = getString("masukan username follower yang akan dihapus : ");
+                    DeleteFollower(L_F, GetFollower(L_F, username));
+                    addressCeleb P = first (L_C);
+                    while (P != nullptr) {
+                        DeleteFollower(info(P).Followers, GetFollower(info(P).Followers, username));    
+                        P = next(P);            
+                    }
+                    substate = 0;
+                } else if (substate == 5){
+                    substate = -1;
+                } else substate = 0;
+            }
+                    
             state = 0;
         } else 
         if (state == 2) {
-            string username = getString("masukan username celeb yang akan dihapus : ");
-            DeleteCeleb(L_C, GetCeleb(L_C, username));
-            addressFollower P = first (L_F);
-            while (P != nullptr) {
-                DeleteCeleb(info(P).Following, GetCeleb(info(P).Following, username));    
-                P = next(P);            
-            }
-            state = 0;    
-        } else
-        if (state == 3) {
             viewAllCeleb(L_C);
             Continue();
             state = 0;
         } else
-        if (state == 4) {
+        if (state == 3) {
             addressCeleb C = GetCeleb(L_C, getString("enter the celeb username : "));
             addressFollower F = GetFollower(info(C).Followers, getString("enter the follower name : "));
             if (F == nullptr) 
@@ -102,26 +135,22 @@ int main () {
             Continue();
             state = 0;
         } else 
-        if (state == 5) {
+        if (state == 4) {
             viewCeleb(L_C, info(last(L_C)).username);
             Continue();
             state = 0;
-        }
-        if (state == 6) {
-            InsertLastFollower (L_F, AllocateFollower(getString("Masukan username follower baru : ")));        
-            state = 0;   
-        }
-        if (state == 7) {
+        } else
+        if (state == 5) {
             Follow(getString("masukan username celeb : "), getString("masukan username follower : "));
             state = 0;
         } else 
-        if (state == 8) {
+        if (state == 6) {
             addressFollower F = first(L_F);
 
             while (F != nullptr) {
 
                 if(info(F).numberofFollowing > 3) {
-                    viewAllCeleb(info(F).Following);
+                    cout << info(F).username;
                 }
 
                 F = next(F);
@@ -129,7 +158,7 @@ int main () {
             Continue();
             state = 0;            
         }
-        if (state == 9){
+        if (state == 7){
             state = -1;
         } 
 
